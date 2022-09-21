6fb73896933b4131e50a19f40e3b34b9f8c2e55a
harrismalik98 <mharrismalik98@gmail.com>
2022-09-21 13:33:09 +0500

    Updates

---
+++ precache-manifest.632940b164c241458e81955318817712.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac4aecef18d02d1ca34c1115ca31918",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "f3ce8723f8228f7fea60",
+    "url": "/KeeperApp/static/js/2.69b2c6aa.chunk.js"
+  },
+  {
+    "revision": "68c705005540275d507b",
+    "url": "/KeeperApp/static/js/main.e801533f.chunk.js"
+  },
+  {
+    "revision": "ad81aa77cc91800000fc",
+    "url": "/KeeperApp/static/js/runtime-main.854af2fe.js"
+  }
+]);
